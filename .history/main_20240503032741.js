586f4d785c91e0a7cb49a576a4bb647dc4253e51
Brian Ho <bh1525@nyu.edu>
2024-05-03 03:28:30 -0400

    first

---
+++ .history/main_20240503032741.js
@@ -0,0 +1,81 @@
+import './style.css'
+import * as THREE from 'three'
+import { addBoilerPlateMesh, addStandardMesh } from './addMeshes'
+import { addLight } from './addLights'
+import Model from './Model'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+const scene = new THREE.Scene()
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+const camera = new THREE.PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
+)
+camera.position.set(0, 0, 40)
+
+//Globals
+const meshes = {}
+const lights = {}
+const mixers = []
+const clock = new THREE.Clock()
+const controls = new OrbitControls(camera, renderer.domElement)
+init()
+function init() {
+	renderer.setSize(window.innerWidth, window.innerHeight)
+	document.body.appendChild(renderer.domElement)
+
+	//meshes
+
+	//lights
+	lights.defaultLight = addLight()
+
+	//changes
+	// meshes.default.scale.set(2, 2, 2)
+
+	//scene operations
+	scene.add(meshes.default)
+	scene.add(meshes.standard)
+	scene.add(lights.defaultLight)
+
+	models()
+	resize()
+	animate()
+}
+
+function models() {
+	//How we add models
+	const Aquarium = new Model({
+		url: 'aquarium.glb',
+		name: 'aquarium',
+		meshes: meshes,
+		scene: scene,
+		scale: new THREE.Vector3(0.2, 0.2, 0.2),
+		rotation: new THREE.Vector3(0, Math.PI / 2, 0),
+	})
+	Aquarium.init()
+	const Fish = new Model({
+		url: 'shiny_fish.glb',
+		name: 'fish',
+		meshes: meshes,
+		scene: scene,
+		position: new THREE.Vector3(-5.5, 10.5, 1),
+	})
+	Fish.init()
+}
+
+function resize() {
+	window.addEventListener('resize', () => {
+		renderer.setSize(window.innerWidth, window.innerHeight)
+		camera.aspect = window.innerWidth / window.innerHeight
+		camera.updateProjectionMatrix()
+	})
+}
+
+function animate() {
+	requestAnimationFrame(animate)
+	const delta = clock.getDelta()
+
+	renderer.render(scene, camera)
+}
